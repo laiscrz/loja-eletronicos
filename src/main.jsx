f4d9a4b9d85a13842fbb1b187e96d5aa67317502
laiscrz <lais.albino2003@gmail.com>
2023-09-17 18:13:18 -0300

    add de rotas na main, e add novas dependencias

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import Home from './Router/Home/index.jsx'
+import Error from './Router/Error/Error.jsx'
+import Aparelhos from './Router/Aparelhos/aparelhos.jsx'
+import VisualizarAparelho from './Router/VisualizarAparelho/visualizarAparelho.jsx'
+
+import {createBrowserRouter, RouterProvider,Navigate} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      {        
+        path:'/',
+        element: <Home/>
+
+      },
+      {
+        path: '/Aparelhos',
+        element: <Aparelhos/>
+
+      },
+      {
+        path: '/VisualizarAparelho/:id',
+        element: <VisualizarAparelho/>
+
+      },
+      {
+        path: '/antiga',
+        element: <Navigate to='/'/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
